8a2e32474d52875122934d4aaa4d65807d8070f2
Johann Boumans <johannboumans@gmail.com>
2019-02-11 19:20:25 +0100

    Set up project for ios

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Text>salut la familles</Text>
       </View>
     );
   }
@@ -14,7 +15,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffa07b',
     alignItems: 'center',
     justifyContent: 'center',
   },
