9368897a616129936efd3f494cbc74ae1cf19e55
Sebastien Crepin <seb_crep@hotmail.com>
2019-02-18 17:31:37 +0100

    Sort event by startdate

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -11,7 +11,7 @@ class HomeScreen extends React.Component {
   }
 
   componentDidMount() {
-    let postsUrl = `https://www.googleapis.com/calendar/v3/calendars/ifosupwavre.be_8gvh4v3v8dae5ktb21hisci9h0@group.calendar.google.com/events?key=AIzaSyCTHMnkmKEU6cMQzd6I6qG9LKvttLPf70c&singleEvents=true`;
+    let postsUrl = `https://www.googleapis.com/calendar/v3/calendars/ifosupwavre.be_8gvh4v3v8dae5ktb21hisci9h0@group.calendar.google.com/events?key=AIzaSyCTHMnkmKEU6cMQzd6I6qG9LKvttLPf70c&singleEvents=true&orderBy=startTime`;
     fetch(postsUrl)
       .then(response => response.json())
       .then(response => {
@@ -27,7 +27,7 @@ class HomeScreen extends React.Component {
     }
     return this.state.events.items.map(event => {
       return (
-        <View key={event.id}>
+        <View key={event.id} style={{ marginBottom: 20 }}>
           <Text>{event.summary}</Text>
           <Text>{event.start.date || event.start.dateTime}</Text>
           <Text>{event.end.date || event.end.dateTime}</Text>
