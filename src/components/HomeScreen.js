887d3dd5f43f66bd5ffc6e2f62017901e2e5718f
Sebastien Crepin <seb_crep@hotmail.com>
2019-02-18 16:41:23 +0100

    Display events on homepage

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -11,7 +11,7 @@ class HomeScreen extends React.Component {
   }
 
   componentDidMount() {
-    let postsUrl = `https://www.googleapis.com/calendar/v3/calendars/ifosupwavre.be_8gvh4v3v8dae5ktb21hisci9h0@group.calendar.google.com/events?api=AIzaSyCTHMnkmKEU6cMQzd6I6qG9LKvttLPf70c`;
+    let postsUrl = `https://www.googleapis.com/calendar/v3/calendars/ifosupwavre.be_8gvh4v3v8dae5ktb21hisci9h0@group.calendar.google.com/events?key=AIzaSyCTHMnkmKEU6cMQzd6I6qG9LKvttLPf70c`;
     fetch(postsUrl)
       .then((response) => response.json())
       .then((response) => {
@@ -21,46 +21,27 @@ class HomeScreen extends React.Component {
       });
   }
 
+  renderEvents = () => {
+    if (this.state.events === null) {
+      return false;
+    }
+    return this.state.events.items.map(event => {
+      return (
+        <View key={event.id}>
+          <Text>{event.summary}</Text>
+          <Text>{event.start.date}</Text>
+          <Text>{event.end.date}</Text>
+        </View>
+      );
+    });
+  };
+
   render() {
     console.log(this.state.events);
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Home!</Text>
-        <Calendar
-          // Initially visible month. Default = Date()
-          current={'2012-03-01'}
-          // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-          minDate={'2012-05-10'}
-          // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-          maxDate={'2012-05-30'}
-          // Handler which gets executed on day press. Default = undefined
-          onDayPress={(day) => {console.log('selected day', day);}}
-          // Handler which gets executed on day long press. Default = undefined
-          onDayLongPress={(day) => {console.log('selected day', day);}}
-          // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-          monthFormat={'yyyy MM'}
-          // Handler which gets executed when visible month changes in calendar. Default = undefined
-          onMonthChange={(month) => {console.log('month changed', month);}}
-          // Hide month navigation arrows. Default = false
-          hideArrows={true}
-          // Replace default arrows with custom ones (direction can be 'left' or 'right')
-          renderArrow={(direction) => (<Arrow />)}
-          // Do not show days of other months in month page. Default = false
-          hideExtraDays={true}
-          // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
-          // day from another month that is visible in calendar page. Default = false
-          disableMonthChange={true}
-          // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
-          firstDay={1}
-          // Hide day names. Default = false
-          hideDayNames={true}
-          // Show week numbers to the left. Default = false
-          showWeekNumbers={true}
-          // Handler which gets executed when press arrow icon left. It receive a callback can go back month
-          onPressArrowLeft={substractMonth => substractMonth()}
-          // Handler which gets executed when press arrow icon left. It receive a callback can go next month
-          onPressArrowRight={addMonth => addMonth()}
-        />
+        {this.renderEvents()}
       </View>
     );
   }
