1401b85f46583436351569271071e5dd60d0973b
maxmanhattan <vandenberghe.m2@gmail.com>
2019-02-26 15:04:08 +0100

    update profile

--- src/components/ProfileScreen.js
+++ src/components/ProfileScreen.js
@@ -3,8 +3,42 @@ import { Text, View, Image } from 'react-native';
 
 import { withThemeContext } from '../context/ThemeContext';
 import TopBar from './layout/TopBar';
+//import dataLessons from '../data/lessons.json';
 
 class ProfileScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      lessons: null,
+    };
+  }
+
+  componentDidMount () {
+    let dataCursus = `../data/lessons.json`;
+    fetch(dataCursus)
+      .then()
+
+  }
+
+  renderLessons = () => {
+    if (this.state.lessons === null) {
+      return false;
+    }
+    return this.state.lessons.items.map(lesson => {
+      const style = { color: `${this.props.ThemeProvider.themeStyle.foreground}` };
+      const data = dataLessons
+      return (
+        <View key={lesson.id} style={[{ marginBottom: 20 }]}>
+          <Text style={style}>{data.first || data.second}</Text>
+          <Text style={style}>{data.first.title || data.second.title}</Text>
+          <Text style={style}>{data.first.period || data.second.period}</Text>
+          <Text style={style}>{data.first.credits || data.second.credits}</Text>
+          <Text style={style}>{data.first.professor || data.second.professor}</Text>
+        </View>
+      );
+    });
+  };
 
   render() {
     return (
@@ -12,10 +46,13 @@ class ProfileScreen extends React.Component {
         <View>
           <TopBar title="Profile" />
         </View>
-        <View style={[
-          { flex: 1, alignItems: `center`, justifyContent: `center` },
-          { backgroundColor: `${this.props.ThemeProvider.themeStyle.background}` },
-        ]}>
+        <ScrollView style={[
+            styles.container,
+            { backgroundColor: `${this.props.ThemeProvider.themeStyle.background}` }]}
+          >
+            {this.renderLessons()}
+          </ScrollView>
+        <View >
         </View>
       </View>
     );
