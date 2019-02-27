b8928c91ca83c91f05e9168f10b7eea7a6a26c46
Sebastien Crepin <seb_crep@hotmail.com>
2019-02-27 16:23:58 +0100

    Remove calendar

--- src/components/home/EventsCalendar.js
+++ src/components/home/EventsCalendar.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { CalendarList, LocaleConfig } from 'react-native-calendars';
+import { View, Text } from 'react-native';
 import PropTypes from 'prop-types';
 
-import { config } from '../../locale/calendar';
 import { withThemeContext } from '../../context/ThemeContext';
-
-LocaleConfig.locales['fr'] = config;
-LocaleConfig.defaultLocale = 'fr';
 
 class EventsCalendar extends Component {
   render() {
     const { background, foreground } = this.props.ThemeProvider.themeStyle;
     return (
       <View>
-        <CalendarList theme={{
-          calendarBackground: `${background}`,
-          textSectionTitleColor: `${foreground}`,
-          monthTextColor: `${foreground}`,
-        }} />
+        <Text>Calendrier</Text>
       </View>
     );
   }
