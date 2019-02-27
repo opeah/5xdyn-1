8f67a3fb55ddc764cb698f27eb6a2e405d8c2835
Johann Boumans <johannboumans@gmail.com>
2019-02-27 17:21:37 +0100

    shit2

--- App.js
+++ App.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 import Menu from './src/components/menu/Navigation';
 import { ThemeProvider } from './src/context/ThemeContext';
 
+
+
 class App extends Component {
+
+
   render() {
     return (
       <ThemeProvider>
