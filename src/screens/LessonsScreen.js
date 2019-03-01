84525e72ed2efc6dcf1cc1236288c757e28baf12
Sebastien Crepin <seb_crep@hotmail.com>
2019-03-01 09:48:00 +0100

    Add average for lessons

--- src/screens/LessonsScreen.js
+++ src/screens/LessonsScreen.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import * as PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 
@@ -7,8 +8,26 @@ import Lesson from '../components/lessons/Lesson';
 
 import { withAppContext } from '../context/AppContext';
 
-const LessonsScreen = ({ Store }) => {
-  const renderLessons = () => {
+class LessonsScreen extends Component {
+  state = {
+    average: null,
+  };
+
+  componentDidMount() {
+    this.calculateAverage();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.Store.lessons !== this.props.Store.lessons) {
+      this.calculateAverage();
+    }
+    if (prevProps.Store.currentYear !== this.props.Store.currentYear) {
+      this.calculateAverage();
+    }
+  }
+
+  renderLessons = () => {
+    const { Store } = this.props;
     if (Store.lessons === null) {
       return false;
     }
@@ -16,19 +35,54 @@ const LessonsScreen = ({ Store }) => {
       <Lesson lesson={lesson} key={index} id={index} />);
   };
 
-  return (
-    <SafeAreaView style={{ height: `100%`, backgroundColor: `${Store.themeStyle.background}` }}>
-      <Header title="Cours" />
-      <ScrollView style={styles.lessonsList}>
-        <View style={{ ...styles.lessonsList__container }}>
-          {renderLessons()}
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  calculateAverage = () => {
+    const { Store } = this.props;
+    const notes = [];
+    Store.lessons[Store.currentYear].map((lesson, index) => {
+      if (lesson.note !== undefined) {
+        notes.push(lesson.note);
+      }
+    });
+    const sum = notes.reduce((item, a) => item + a);
+    const average = sum / (notes.length);
+    this.setState({ average });
+  };
+
+  render() {
+    let { Store } = this.props;
+
+    return (
+      <SafeAreaView style={{ height: `100%`, backgroundColor: `${Store.themeStyle.background}` }}>
+        <Header title="Cours" />
+        <ScrollView style={styles.lessonsList}>
+          <View style={styles.Lessons__average}>
+            <Text style={{
+              ...styles.Lessons__average__text,
+              color: `${Store.themeStyle.foreground}`,
+            }}>
+              {this.state.average !== null ? `Moyenne : ${this.state.average}` : `Entrez une note`}
+            </Text>
+          </View>
+          <View style={{ ...styles.lessonsList__container }}>
+            {this.renderLessons()}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
+}
+
+LessonsScreen.propTypes = { Store: PropTypes.any };
 
 const styles = StyleSheet.create({
+  Lessons__average: {
+    marginBottom: 20,
+  },
+  Lessons__average__text: {
+    textAlign: `center`,
+    fontWeight: `800`,
+    fontSize: 20,
+  },
   lessonsList: {
     padding: 20,
     marginBottom: -40,
